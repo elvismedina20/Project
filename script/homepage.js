9b5ce4c9cfa15e6f6edff57889f8e53ef3a5d10c
elvismedina20 <elvismedina1011@gmail.com>
2025-04-23 09:35:28 -0500

    Update and rename script.js to homepage.js

---
+++ script/homepage.js
@@ -0,0 +1,11 @@
+document.getElementById('homeBtn').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+
+document.getElementById('playBtn').addEventListener('click', function() {
+    window.location.href = 'game.html';
+});
+
+document.getElementById('devlogBtn').addEventListener('click', function() {
+    window.location.href = 'devlog.html';
+});
