818f2ade6d3ba8f824ffe1f880c7d10f693a59dc
elvismedina20 <elvismedina1011@gmail.com>
2025-04-23 09:35:57 -0500

    Create devlog.js

---
+++ script/devlog.js
@@ -0,0 +1,7 @@
+document.getElementById('homeBtn').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+
+document.getElementById('playBtn').addEventListener('click', function() {
+    window.location.href = 'game.html';
+});
