52e24ac3de976595c177f7f2fb48e9240163bfab
elvismedina20 <elvismedina1011@gmail.com>
2025-04-23 09:29:50 -0500

    Create script.js

---
+++ script/script.js
@@ -0,0 +1,11 @@
+document.getElementById('homeBtn').addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
+
+document.getElementById('playBtn').addEventListener('click', () => {
+    window.location.href = 'game.html';
+});
+
+document.getElementById('devlogBtn').addEventListener('click', () => {
+    window.location.href = 'devlog.html';
+});
